a5829fae4a338cf8e5b3c73c4129a23dba9f3422
Anderson de Carvalho <anderson.analistati@gmail.com>
2018-01-30 03:15:39 -0200

    react-native-camera

--- src/screens/Ticket/index.js
+++ src/screens/Ticket/index.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import Camera from 'react-native-camera';
 import GlobalStyle from '../../global/Styles';
-// import Camera from 'react-native-camera';
 
 export default class Ticket extends Component {
   render() {
@@ -26,10 +26,37 @@ export default class Ticket extends Component {
             alignItems: 'center'
           }}
         >
-          <Text style={styles.welcome}>Ticket</Text>
+          <View style={{ width: 200, height: 200 }}>
+            <Camera
+              ref={cam => {
+                this.camera = cam;
+              }}
+              style={styles.preview}
+              aspect={Camera.constants.Aspect.fill}
+            >
+              <Text style={styles.capture} onPress={this.takePicture.bind(this)}>
+                [CAPTURE]
+              </Text>
+            </Camera>
+          </View>
         </View>
       </View>
     );
+  }
+
+  takePicture() {
+    const options = {};
+    //options.location = ...
+    this.camera
+      .capture({
+        metadata: options
+      })
+      .then(data => {
+        console.log('data');
+      })
+      .catch(err => {
+        console.error(err);
+      });
   }
 }
 
@@ -47,5 +74,18 @@ const styles = StyleSheet.create({
     textAlign: 'center',
     color: '#333333',
     marginBottom: 5
+  },
+  preview: {
+    flex: 1,
+    justifyContent: 'flex-end',
+    alignItems: 'center'
+  },
+  capture: {
+    flex: 0,
+    backgroundColor: '#fff',
+    borderRadius: 5,
+    color: '#000',
+    padding: 10,
+    margin: 40
   }
 });
