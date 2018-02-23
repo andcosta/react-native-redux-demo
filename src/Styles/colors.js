ec7171e08e0a706c81b15a2214d8d3a8317b1cd3
Anderson de Carvalho <andersonforfan@gmail.com>
2018-02-23 20:44:51 -0300

    Color layout

--- src/Styles/colors.js
+++ src/Styles/colors.js
@@ -1,14 +1,5 @@
 const colors = {
 
-  // global
-  transparent: 'rgba(0, 0, 0, 0)',
-  fontDarker: rgba(17, 17, 17, 1),
-  fontDark: 'rgba(51, 51, 51, 1)',
-  fontRegular: rgba(102, 102, 102, 1),
-  fontLight: rgba(192, 192, 192, 1),
-  fontLighter: rgba(238, 238, 238, 1),
-
-
   // screen
   screenBackground: 'rgba(245, 245, 251, 1)',
 
